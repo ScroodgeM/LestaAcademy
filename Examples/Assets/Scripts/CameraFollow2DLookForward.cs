31f7c167cec74f7d0cb7f83c5152e2d0f5139dfe
Paul Vladimirov <dr.scroodge@gmail.com>
2021-01-28 02:45:06 +0300

    +

--- Examples/Assets/Scripts/CameraFollow2DLookForward.cs
+++ Examples/Assets/Scripts/CameraFollow2DLookForward.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 
-public class CameraFollow2DLerpSmooth : MonoBehaviour
+public class CameraFollow2DLookForward : MonoBehaviour
 {
     [SerializeField] private Vector3 cameraOffset;
     [SerializeField] private Transform target;
     [SerializeField] private float followSpeed;
+    [SerializeField] private float forwardDistance;
 
     private void LateUpdate()
     {
         Vector3 oldPos = transform.position;
-        Vector3 newPos = target.position + cameraOffset;
+        Vector3 newPos = target.position + cameraOffset + target.forward * forwardDistance;
         float lerp = Time.deltaTime * followSpeed;
 
         transform.position = Vector3.Lerp(oldPos, newPos, lerp);
