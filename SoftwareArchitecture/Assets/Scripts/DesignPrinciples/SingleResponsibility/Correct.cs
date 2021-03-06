a11bc041a0c78dde3b81b357d5ec28e9f272bde9
Paul Vladimirov <dr.scroodge@gmail.com>
2021-03-06 20:46:29 +0300

    +

---
+++ SoftwareArchitecture/Assets/Scripts/DesignPrinciples/SingleResponsibility/Correct.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WGADemo.DesignPrinciples.SingleResponsibility.Correct
+{
+    public class Item
+    {
+        private string name;
+        public string Name => Name;
+    }
+
+    public class UserInterface
+    {
+        public void PlayPopup(string text, Color color)
+        {
+            GameObject prefab = Resources.Load<GameObject>("UIPopups/Popup");
+            GameObject instance = GameObject.Instantiate(prefab);
+            instance.GetComponent<Text>().text = text;
+            instance.GetComponent<Text>().color = color;
+            instance.GetComponent<Animator>().Play("move_up_and_fade_out");
+        }
+    }
+
+    public class SaveGameController
+    {
+        public void IncreaseItemCounter(Item item)
+        {
+            int oldItemCount = PlayerPrefs.GetInt($"item_{item.Name}_count");
+            PlayerPrefs.SetInt($"item_{item.Name}_count", oldItemCount + 1);
+        }
+    }
+
+    public class Scenario
+    {
+        private PlayerCharacter playerCharacter;
+        private UserInterface userInterface;
+        private SaveGameController saveGameController;
+
+        public void ProcessPickItemCommand(Item item)
+        {
+            userInterface.PlayPopup(item.Name, Color.white);
+
+            playerCharacter.ProcessPickedItem(item);
+
+            if (playerCharacter.IsOveloaded == true)
+            {
+                userInterface.PlayPopup("Too many items, can't move", Color.red);
+            }
+
+            saveGameController.IncreaseItemCounter(item);
+        }
+    }
+
+    public class AvatarView
+    {
+        private Transform avatarTransform;
+        private Animator animator;
+
+        public void PlayPickItemAnimation()
+        {
+            animator.Play("pick_item_animation");
+        }
+
+        public void SetHelm(bool hasHelm)
+        {
+            if (hasHelm == true)
+            {
+                GameObject helmPrefab = Resources.Load<GameObject>("ItemModels/Helm");
+                GameObject helmInstance = GameObject.Instantiate(helmPrefab, avatarTransform);
+            }
+        }
+    }
+
+    public class PlayerInventory
+    {
+        private List<Item> items;
+        private int maxItemsCount;
+        private Item headItem;
+
+        public void AddItem(Item item)
+        {
+            items.Add(item);
+
+            if (headItem == null && item.Name == "Helm")
+            {
+                headItem = item;
+            }
+        }
+
+        public bool IsOveloaded => items.Count > maxItemsCount;
+
+        public bool HasHelm => headItem != null;
+    }
+
+    public class PlayerCharacter
+    {
+        private AvatarView avatarView;
+        private PlayerInventory inventory;
+        private bool canMove;
+
+        public bool IsOveloaded => inventory.IsOveloaded;
+
+        public void ProcessPickedItem(Item item)
+        {
+            inventory.AddItem(item);
+
+            canMove = inventory.IsOveloaded == false;
+
+            avatarView.PlayPickItemAnimation();
+            avatarView.SetHelm(inventory.HasHelm);
+        }
+    }
+}
---
+++ SoftwareArchitecture/Assets/Scripts/DesignPrinciples/SingleResponsibility/Wrong.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WGADemo.DesignPrinciples.SingleResponsibility.Wrong
+{
+    public class Item
+    {
+        private string name;
+        public string Name => Name;
+    }
+
+    public class PlayerCharacter
+    {
+        private Transform playerAvatar;
+        private Animator animator;
+        private List<Item> items;
+        private int maxItemsCount;
+        private Item headItem;
+        private bool canMove;
+
+        public void ProcessPickItemCommand(Item item)
+        {
+            animator.Play("pick_item_animation");
+
+            if (headItem == null && item.Name == "Helm")
+            {
+                headItem = item;
+                GameObject helmPrefab = Resources.Load<GameObject>("ItemModels/Helm");
+                GameObject helmInstance = GameObject.Instantiate(helmPrefab, playerAvatar);
+            }
+
+            GameObject popupPrefab = Resources.Load<GameObject>("UIPopups/PickedItemPopup");
+            GameObject popupInstance = GameObject.Instantiate(popupPrefab);
+            popupInstance.GetComponent<Text>().text = item.Name;
+            popupInstance.GetComponent<Animator>().Play("move_up_and_fade_out");
+
+            items.Add(item);
+            if (items.Count >= maxItemsCount)
+            {
+                canMove = false;
+                GameObject cantMovePopupPrefab = Resources.Load<GameObject>("UIPopups/TooManyItemsCantMove");
+                GameObject cantMovePopupInstance = GameObject.Instantiate(cantMovePopupPrefab);
+                cantMovePopupInstance.GetComponent<Text>().text = "Too many items, can't move";
+                cantMovePopupInstance.GetComponent<Text>().color = Color.red;
+                cantMovePopupInstance.GetComponent<Animator>().Play("move_up_and_fade_out");
+            }
+
+            int oldItemCount = PlayerPrefs.GetInt($"item_{item.Name}_count");
+            PlayerPrefs.SetInt($"item_{item.Name}_count", oldItemCount + 1);
+        }
+    }
+}
