82200205d94d206a212aeaac4540f2cbc98e279a
Paul Vladimirov <dr.scroodge@gmail.com>
2021-03-06 19:35:29 +0300

    +

---
+++ SoftwareArchitecture/Assets/Scripts/DesignPrinciples/OpenClosed/Correct.cs
@@ -0,0 +1,54 @@
+﻿
+namespace WGADemo.DesignPrinciples.OpenClosed.Correct
+{
+    public class Armor
+    {
+        public float physicalAbsorb;
+        public float physicalResist;
+        public float fireResist;
+        public float iceResist;
+    }
+
+    public abstract class Damage
+    {
+        public float points;
+
+        public abstract float GetActualDamage(Armor armor);
+    }
+
+    public class PhysicalDamage : Damage
+    {
+        public override float GetActualDamage(Armor armor) => (points - armor.physicalAbsorb) * (1f - armor.physicalResist);
+    }
+
+    public class MagicDamage : Damage
+    {
+        public override float GetActualDamage(Armor armor) => points;
+    }
+
+    public class FireDamage : Damage
+    {
+        public override float GetActualDamage(Armor armor) => points * (1f - armor.fireResist);
+    }
+
+    public class IceDamage : Damage
+    {
+        public override float GetActualDamage(Armor armor) => points * (1f - armor.iceResist);
+    }
+
+    public class Unit
+    {
+        private float health;
+        private Armor armor;
+
+        public void Damage(Damage damage)
+        {
+            float actualDamage = damage.GetActualDamage(armor);
+
+            if (actualDamage > 0f)
+            {
+                health -= actualDamage;
+            }
+        }
+    }
+}
---
+++ SoftwareArchitecture/Assets/Scripts/DesignPrinciples/OpenClosed/Wrong.cs
@@ -0,0 +1,56 @@
+﻿
+namespace WGADemo.DesignPrinciples.OpenClosed.Wrong
+{
+    public enum DamageType
+    {
+        Physical, Magic, Fire, Ice
+    }
+
+    public class Armor
+    {
+        public float physicalAbsorb;
+        public float physicalResist;
+        public float fireResist;
+        public float iceResist;
+    }
+
+    public class Damage
+    {
+        public DamageType type;
+        public float points;
+    }
+
+    public class Unit
+    {
+        private float health;
+        private Armor armor;
+
+        public void Damage(Damage damage)
+        {
+            float actualDamage;
+
+            switch (damage.type)
+            {
+                case DamageType.Physical:
+                    actualDamage = (damage.points - armor.physicalAbsorb) * (1f - armor.physicalResist);
+                    break;
+                case DamageType.Magic:
+                    actualDamage = damage.points;
+                    break;
+                case DamageType.Fire:
+                    actualDamage = damage.points * (1f - armor.fireResist);
+                    break;
+                case DamageType.Ice:
+                    actualDamage = damage.points * (1f - armor.iceResist);
+                    break;
+                default:
+                    return;
+            }
+
+            if (actualDamage > 0f)
+            {
+                health -= actualDamage;
+            }
+        }
+    }
+}
