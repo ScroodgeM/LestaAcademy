80f9fb286a80001ad26536299cae5faef96fc767
Paul Vladimirov <dr.scroodge@gmail.com>
2021-03-06 18:22:52 +0300

    +demos

---
+++ SoftwareArchitecture/Assets/Scripts/DesignPrinciples/DontRepeatYourself/Correct.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+
+namespace WGADemo.Correct
+{
+    public abstract class UnitBehaviour
+    {
+        protected Unit GetLowestHealthUnit(List<Unit> units)
+        {
+            Unit result = null;
+
+            foreach (Unit unit in units)
+            {
+                if (result == null || unit.GetHealth() < result.GetHealth())
+                {
+                    result = unit;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public class AttackUnitBehaviour: UnitBehaviour
+    {
+        public void UseSkill(List<Unit> allEnemies)
+        {
+            Unit lowestHealthUnit = GetLowestHealthUnit(allEnemies);
+
+            Attack(lowestHealthUnit);
+        }
+
+        private void Attack(Unit unit)
+        {
+            unit.HandleAttack(10f);
+        }
+    }
+
+    public class HealerUnitBehaviour: UnitBehaviour
+    {
+        public void UseSkill(List<Unit> allAllies)
+        {
+            Unit lowestHealthUnit = GetLowestHealthUnit(allAllies);
+
+            if (lowestHealthUnit.GetHealth() < Unit.MaxHealth)
+            {
+                Heal(lowestHealthUnit);
+            }
+        }
+
+        private void Heal(Unit unit)
+        {
+            unit.HandleHeal(10f);
+        }
+    }
+
+    public class Unit
+    {
+        public const float MaxHealth = 100f;
+
+        private float health;
+
+        public float GetHealth() => health;
+
+        public void HandleHeal(float healthPoints)
+        {
+            health += healthPoints;
+            if (health > MaxHealth) { health = MaxHealth; }
+        }
+
+        public void HandleAttack(float damagePoints)
+        {
+            health -= damagePoints;
+        }
+    }
+}
