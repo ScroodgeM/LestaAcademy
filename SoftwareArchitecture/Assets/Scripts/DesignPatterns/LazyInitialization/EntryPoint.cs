df950e10b8a1d919e9ebc8c57bf4da3e6ff58472
pvladimirov <dr.scroodge@gmail.com>
2021-03-09 16:26:50 +0300

    lazy initialization

---
+++ SoftwareArchitecture/Assets/Scripts/DesignPatterns/LazyInitialization/EntryPoint.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace WGADemo.DesignPatterns.LazyInitialization
+{
+    public class EntryPoint : MonoBehaviour
+    {
+        private static EntryPoint instance;
+
+        public static EntryPoint Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    GameObject entryPointHolder = new GameObject();
+                    DontDestroyOnLoad(entryPointHolder);
+
+                    instance = entryPointHolder.AddComponent<EntryPoint>();
+                }
+
+                return instance;
+            }
+        }
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Debug.LogError($"instance of {nameof(EntryPoint)} already exists");
+            }
+        }
+
+        private bool todaysWinNumberCalculated = false;
+        private int todaysWinNumber;
+
+        public int GetTodaysWinNumber()
+        {
+            if (todaysWinNumberCalculated == false)
+            {
+                // very hard calculation for sure
+                todaysWinNumber = UnityEngine.Random.Range(10, 100);
+
+                todaysWinNumberCalculated = true;
+            }
+
+            return todaysWinNumber;
+        }
+    }
+}
