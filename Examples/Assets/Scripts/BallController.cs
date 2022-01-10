c83c24c4979f8359ae1b087069f3e1208af1f6c8
CORP\p_vladimirov <p_vladimirov@wargaming.net>
2022-01-10 13:01:31 +0300

    scene adjusted

--- Examples/Assets/Scripts/BallController.cs
+++ Examples/Assets/Scripts/BallController.cs
@@ -1,13 +1,51 @@
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BallController : MonoBehaviour
 {
-    private void Update()
+    [SerializeField] private PlayerInput playerInput;
+
+    private void Awake()
     {
-        Vector3 moveCommand;
-        moveCommand.x = Input.GetAxis("Horizontal");
-        moveCommand.y = Input.GetAxis("Jump");
-        moveCommand.z = Input.GetAxis("Vertical");
-        transform.position = moveCommand;
+        playerInput.onActionTriggered += OnPlayerInputActionTriggered;
+    }
+
+    private void OnPlayerInputActionTriggered(InputAction.CallbackContext context)
+    {
+        switch (context.action.name)
+        {
+            case "Move":
+                Vector2 moveCommand = context.action.ReadValue<Vector2>();
+                HandleMoveCommand(moveCommand);
+                break;
+
+            case "Jump":
+                switch (context.action.phase)
+                {
+                    case InputActionPhase.Started:
+                        HandleJumpCommand(true);
+                        break;
+                    case InputActionPhase.Canceled:
+                        HandleJumpCommand(false);
+                        break;
+                }
+                break;
+        }
+    }
+
+    private void HandleMoveCommand(Vector2 moveCommand)
+    {
+        Vector3 position = transform.position;
+        position.x = moveCommand.x;
+        position.z = moveCommand.y;
+        transform.position = position;
+    }
+
+    private void HandleJumpCommand(bool jumping)
+    {
+        Vector3 position = transform.position;
+        position.y = jumping ? 1 : 0;
+        transform.position = position;
     }
 }
--- Examples/Assets/Scripts/ButtonDebug.cs
+++ Examples/Assets/Scripts/ButtonDebug.cs
@@ -1,6 +1,8 @@
 
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class ButtonDebug : MonoBehaviour
 {
@@ -15,7 +17,40 @@ public class ButtonDebug : MonoBehaviour
 
     private void Update()
     {
-        bool keyPressed = Input.GetKey(keyToListen);
+        bool keyPressed = GetKeyControl(keyToListen).isPressed;
         pressedStateIndicator.SetActive(keyPressed);
+    }
+
+    private static KeyControl GetKeyControl(KeyCode keyCode)
+    {
+        switch (keyCode)
+        {
+            case KeyCode.W:
+                return Keyboard.current.wKey;
+            case KeyCode.A:
+                return Keyboard.current.aKey;
+            case KeyCode.S:
+                return Keyboard.current.sKey;
+            case KeyCode.D:
+                return Keyboard.current.dKey;
+
+            case KeyCode.Space:
+                return Keyboard.current.spaceKey;
+            case KeyCode.LeftControl:
+                return Keyboard.current.leftCtrlKey;
+
+            case KeyCode.LeftArrow:
+                return Keyboard.current.leftArrowKey;
+            case KeyCode.RightArrow:
+                return Keyboard.current.rightArrowKey;
+            case KeyCode.UpArrow:
+                return Keyboard.current.upArrowKey;
+            case KeyCode.DownArrow:
+                return Keyboard.current.downArrowKey;
+
+            default:
+                Debug.LogError($"key {keyCode} not mapped");
+                return default;
+        }
     }
 }
