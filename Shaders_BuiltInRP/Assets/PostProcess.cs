c04bfe7664e7129e3165953b5128a50a9ab7b8b5
CORP\p_vladimirov <p_vladimirov@wargaming.net>
2022-01-27 09:56:38 +0300

    screen shader demo

---
+++ Shaders_BuiltInRP/Assets/PostProcess.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PostProcess : MonoBehaviour
+{
+    // A Material with the Unity shader you want to process the image with
+    public Material mat;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        // Read pixels from the source RenderTexture, apply the material, copy the updated results to the destination RenderTexture
+        Graphics.Blit(src, dest, mat);
+    }
+}
